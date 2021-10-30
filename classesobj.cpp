5db7cad0c72846686ae4fa64463e8590203fcafc
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2021-10-30 09:39:52 +0530

    Added program Sleep Sort, Polynomial Regression, classesobj, secondsmallestusingunordered

---
+++ classesobj.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class ABC{
+  public:
+    int x;
+    string y;
+
+    int sq(int x);
+
+  };
+
+double ABC::sq(int x){
+  return x*x;
+}
+    int main(){
+      ABC a;
+      ABC b;
+
+      int num1 = a.sqaure(x);
+      cout<<"Sqaure :"<<num1<<endl;
+      cout<<"Cube :"<num2<<endl;
+    }
